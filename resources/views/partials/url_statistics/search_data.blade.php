a1f7be390c8d510e9e7b409826734229ad605245
John Paul Belga <belgajohnpauldimaano@gmail.com>
2017-12-07 08:31:39 +0800

    master : checking for existence of data

--- resources/views/partials/url_statistics/search_data.blade.php
+++ resources/views/partials/url_statistics/search_data.blade.php
@@ -1,53 +1,61 @@
 
+
 <h2>URL Information</h2>
-<table class="table table-bordered table-striped">
-    <tr>
-        <th>Actual Link</th>
-        <td>{{ $Link->actual_link }}</td>
-    </tr>
-    <tr>
-        <th>Shortened Link</th>
-        <td>{{ $Link->full_shortened_link }}</td>
-    </tr>
-</table>
-
+@if($Link)
+    <table class="table table-bordered table-striped">
+        <tr>
+            <th>Actual Link</th>
+            <td>{{ $Link->actual_link }}</td>
+        </tr>
+        <tr>
+            <th>Shortened Link</th>
+            <td>{{ $Link->full_shortened_link }}</td>
+        </tr>
+    </table>
+@else
+    No Data Faound
+@endif
 <h2>Visitor's Informtion</h2>
-<table class="table table-bordered table-striped">
-    <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Email</th>
-        <th>Address</th>
-        <th>Citizenship</th>
-        <th>Country</th>
-    </tr>
-    <tbody>
-        @if ($Link->user_visits)  
-            {{--  {{ json_encode($Link->user_visits) }}  --}}
-            @foreach($Link->user_visits as $data)
-            {{--  {{ json_encode($user) }}  --}}
-                <tr>
-                    <td>
-                        {{ $data->user->name }} 
-                    </td>
-                    <td>
-                        {{ $data->user->age }} 
-                    </td>
-                    <td>
-                        {{ $data->user->email }} 
-                    </td>
-                    <td>
-                        {{ $data->user->address }} 
-                    </td>
-                    <td>
-                        {{ $data->user->citizenship->citizenship_name }} 
-                    </td>
-                    <td>
-                        {{ $data->user->country->country_name }} 
-                    </td>
-                </tr>
-            @endforeach
+@if($Link)
+    <table class="table table-bordered table-striped">
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Citizenship</th>
+            <th>Country</th>
+        </tr>
+        <tbody>
+            @if ($Link->user_visits)  
+                {{--  {{ json_encode($Link->user_visits) }}  --}}
+                @foreach($Link->user_visits as $data)
+                {{--  {{ json_encode($user) }}  --}}
+                    <tr>
+                        <td>
+                            {{ $data->user->name }} 
+                        </td>
+                        <td>
+                            {{ $data->user->age }} 
+                        </td>
+                        <td>
+                            {{ $data->user->email }} 
+                        </td>
+                        <td>
+                            {{ $data->user->address }} 
+                        </td>
+                        <td>
+                            {{ $data->user->citizenship->citizenship_name }} 
+                        </td>
+                        <td>
+                            {{ $data->user->country->country_name }} 
+                        </td>
+                    </tr>
+                @endforeach
 
-        @endif
-    </tbody>
-</table>
+            @endif
+        </tbody>
+    </table>
+@else
+    No Data Faound
+@endif
